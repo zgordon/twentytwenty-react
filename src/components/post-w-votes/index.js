b99a7fee5a475f940de4d049cb327cdee75e1ac2
Zac Gordon <email@zacgordon.com>
2019-12-10 16:01:12 -0500

    Cleaning up Posts component

--- src/components/post-w-votes/index.js
+++ src/components/post-w-votes/index.js
@@ -1,3 +1,5 @@
+const { useState } = wp.element;
+
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 
@@ -20,7 +22,24 @@ const Posts = () => {
   if (loading) return <p>Loading posts...</p>;
   if (error) return <p>Error :(</p>;
 
+  // const [posts, setPosts] = useState([]);
+
+  // const posts = [...data.posts.nodes];
+
+  // const sortedPosts = data.posts.nodes
+  //   .sort((a, b) => a.votes < b.votes)
+  //   .map(({ id, title, votes }) => (
+  //     <li key={id}>
+  //       <button>&#8679;</button>
+  //       {votes}
+  //       <button>&#8681;</button>
+  //       <h3>{title}</h3>
+  //     </li>
+  //   ));
+  // return <ul>{sortedPosts}</ul>;
+
   return posts.map(({ postId, title }) => <h3 key={postId}>{title}</h3>);
+  // return <ul>{sortedPosts}</ul>;
 };
 
 export default Posts;
